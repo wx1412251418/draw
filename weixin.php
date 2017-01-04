89cd173a260f53d2fb06abcb2613ac1eb89d1dfe
wuxiang <1412251418@qq.com>
2017-01-04 10:38:48 +0800

    初始化

---
+++ weixin.php
@@ -0,0 +1,33 @@
+<?php
+
+header('Content-type:text');
+
+define("TOKEN", "weixin");
+$wechatObj = new wechatCallbackapiTest();
+if (!isset($_GET['echostr'])) {
+    $wechatObj->responseMsg();
+}else{
+    $wechatObj->valid();
+}
+
+class wechatCallbackapiTest
+{
+    //验证签名
+    public function valid()
+    {
+        $echoStr = $_GET["echostr"];
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
+        if($tmpStr == $signature){
+            echo $echoStr;
+            exit;
+        }
+    }
+}
+?>
